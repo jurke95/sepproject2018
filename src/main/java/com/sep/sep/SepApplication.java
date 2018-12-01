366ccdef36a03fbd30e64892552bdb5ec77634b3
jurke95 <markivicius@hotmail.com>
2018-12-01 21:50:56 +0100

    firstcommit

---
+++ src/main/java/com/sep/sep/SepApplication.java
@@ -0,0 +1,12 @@
+package com.sep.sep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sep/sep/SepApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sep.sep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SepApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
